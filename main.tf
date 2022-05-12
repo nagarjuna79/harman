e5bb705b9f4aa38f29025d55e5ad8a8e044b2ce3
nagarjuna79 <104488122+nagarjuna79@users.noreply.github.com>
2022-05-12 12:38:07 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -284,8 +284,7 @@ resource "aws_launch_configuration" "webserver-launch-config" {
             volume_size = 5
             encrypted   = true
         }
-       
-
+  
   lifecycle {
     create_before_destroy = true
   }
@@ -296,15 +295,25 @@ resource "aws_launch_configuration" "webserver-launch-config" {
 # Create Auto Scaling Group
 resource "aws_autoscaling_group" "Demo-ASG-tf" {
   name		     = "Demo1-ASG-tf"
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
+  desired_capacity   = 2
+  max_size           = 4
+  min_size           = 2
   force_delete       = true
   depends_on 	     = ["aws_lb.ALB-tf"]
   target_group_arns  =  ["${aws_lb_target_group.TG-tf.arn}"]
   health_check_type  = "EC2"
   launch_configuration = aws_launch_configuration.webserver-launch-config.name
   vpc_zone_identifier = ["${aws_subnet.prv_sub1.id}","${aws_subnet.prv_sub2.id}"]
+  
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      // You probably want more than 50% healthy depending on how much headroom you have
+      min_healthy_percentage = 50
+    }
+    // Depending the triggers you wish to configure, you may not want to include this
+    triggers = ["tag"]
+  }
 
  tag {
     key                 = "Name"
