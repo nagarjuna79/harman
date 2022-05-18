3ff6367d539df1eef4fe6a525e22aa5dae158a73
nagarjuna79 <104488122+nagarjuna79@users.noreply.github.com>
2022-05-18 19:19:01 +0530

    Update iamroles.tf

--- iamroles.tf
+++ iamroles.tf
@@ -1,21 +1,21 @@
 resource "aws_iam_role" "ec2_s3_access_role" {
-  name               = "s3-role-4"
+  name               = "s3-role-7"
   assume_role_policy = "${file("assumepolicy.json")}"
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "test-policy-4"
+  name        = "test-policy-7"
   description = "A test policy"
   policy      = "${file("policys3bucket.json")}"
 }
 
 resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment-4"
+  name       = "test-attachment-8"
   roles      = ["${aws_iam_role.ec2_s3_access_role.name}"]
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
 
 resource "aws_iam_instance_profile" "test_profile" {
-  name  = "test_profile-4"
+  name  = "test_profile-8"
   role = "${aws_iam_role.ec2_s3_access_role.name}"
 }
